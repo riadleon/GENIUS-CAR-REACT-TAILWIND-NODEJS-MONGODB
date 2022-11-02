0db54e2b69f7885042ae2b572a220a405b48a577
Riad Shalahin Leon <105214634+riadleon@users.noreply.github.com>
2022-11-02 16:54:15 +0600

    Style Banner Component, work with text and buttons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import {  RouterProvider } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 import router from './Router/Routes/Routes';
 
 function App() {
 
   return (
-    <div>
+    <div data-theme="light" className='max-w-7xl mx-auto'>
       <RouterProvider router={router} />
     </div>
   );
