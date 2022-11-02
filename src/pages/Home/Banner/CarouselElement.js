31eed06673a68ce8cce61abe2c2202cccfc4ab5a
Riad Shalahin Leon <105214634+riadleon@users.noreply.github.com>
2022-11-02 19:50:32 +0600

    Load Services fake data and display service Card

--- src/pages/Home/Banner/CarouselElement.js
+++ src/pages/Home/Banner/CarouselElement.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './CarouselElement.css'
 
 const CarouselElement = ({ slide }) => {
     const { image, id, prev, next } = slide;
     return (
         <div id={`slide${id}`} className="carousel-item relative w-full">
             <div className='carousel-image'>
-                <img src={image} alt="" className="w-full" />
+                <img src={image} alt="" className="w-full rounded-xl" />
             </div>
             <div className="absolute flex justify-end transform -translate-y-1/2 left-24  top-1/3 ">
                 <h1 className='text-6xl font-bold text-white'>
